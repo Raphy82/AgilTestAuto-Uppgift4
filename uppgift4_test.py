00e78a16619f606b8a45c579eea4e002de6d4579
Raphy82 <raphy.thomas82@gmail.com>
2022-12-09 12:54:46 +0100

    update

--- uppgift4_test.py
+++ uppgift4_test.py
@@ -3,4 +3,19 @@
 '#Hej alla hungriga'
 '#Snart kan vi logga ut!'
 '# Jag vill också skriva!'
+def login():
+    for
+    sdsd
+    defsds
+    ds fdfdsfds
+fdsfdsfdsfsd
+
+def productsite1():
+
+    dictdsdsdas
+    defsad
+    assert das
+    dsa
+
+
 '# Hej på er i efterhand!'
