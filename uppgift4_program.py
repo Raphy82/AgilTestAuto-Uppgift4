160d80c5215ff046ec33ca65bb04bc31b2ff2384
sfallah4 <sofia.fallah@iths.se>
2022-12-29 13:13:59 +0100

    commit email + password + click login

--- uppgift4_program.py
+++ uppgift4_program.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 '# Call the setup fixture'
 @pytest.mark.usefixtures("setup")
@@ -11,25 +12,34 @@ class Systembolaget:
     def __init__(self, driver):
         self.driver = driver
 
-    '#Click on logga in button on the upper right corner'
-    def login_box(self):
-        login_box = self.driver.find_element(By.XPATH, "//button[normalize-space()='Logga in']")
-        login_box.click()
-
     '#Login to the website, insert email'
-    def login_email(self):
-        logins = self.driver.find_element(By.XPATH, "//input[@type='text']").send_keys("Grupp.1.Python@gmail.com")
-        logins.send_keys(Keys.ENTER)
+    def login_email(self, logins):
+        login = self.driver.find_element(By.NAME, "Username")
+        login.send_keys(logins)
+        login.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+    '#Login to the website, insert password'
+    def login_passwd(self, names):
+        name = self.driver.find_element(By.NAME, "Password")
+        name.send_keys(names)
+        name.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+    '#Login to the website, click on login button'
+    def login_button(self):
+        checkbox = self.driver.find_element(By.XPATH, "//button[@class='btn btn-primary button-submit']")
+        checkbox.click()
 
     '#Find item on the website'
-    def find_item(self, skus):
-        sku = self.driver.find_element(By.XPATH, "//input[@placeholder='Sök dryck, land, hållbart val...']")
-        sku.send_keys(skus)
-        sku.send_keys(Keys.ENTER)
-        self.driver.find_element(By.XPATH, "//p[@class='css-w9tb7l e3wog7r1']")
-        sku.click()
+     def find_item(self, skus):
+         sku = self.driver.find_element(By.XPATH, "//input[@placeholder='Sök dryck, land, hållbart val...']")
+         sku.send_keys(skus)
+         sku.send_keys(Keys.ENTER)
+         self.driver.find_element(By.XPATH, "//p[@class='css-w9tb7l e3wog7r1']")
+         sku.click()
 
     '#From product page, add item to basket'
-    def add_item(self):
-        self.driver.find_element(By.XPATH, "//button[normalize-space()='Lägg i varukorg']").click()
+     def add_item(self):
+         self.driver.find_element(By.XPATH, "//button[normalize-space()='Lägg i varukorg']").click()
 
--- uppgift4_test.py
+++ uppgift4_test.py
@@ -9,16 +9,23 @@ class TestSystembolaget:
 
     def test_email(self):
         login1 = Systembolaget(self.driver)
-        login1.find_item("Grupp.1.Python@gmail.com")
-        email = self.driver.find_element(By.NAME, "Username")
-        email = email.text
-        assert "Grupp.1.Python@gmail.com" in email
+        login1.login_email("Grupp.1.Python@gmail.com")
+        username = self.driver.find_element(By.NAME, "Username")
+        username = username.text
+        assert "Grupp.1.Python@gmail.com" in username
 
-    def test_demo(self):
-        sku1 = Systembolaget(self.driver)
-        sku1.find_item("2722")
-        demoartikel = self.driver.find_element(By.XPATH, "//div[@class='css-8zpafe e3whs8q0']//p[@class='css-12l74ml er6ap680']")
-        demoartikel = demoartikel.text
-        assert "2722" in demoartikel
+    def test_password(self):
+        name1 = Systembolaget(self.driver)
+        name1.login_passwd("Grupp1Python")
+        password = self.driver.find_element(By.NAME, "Password")
+        password = password.text
+        assert "Grupp1Python" in password
+
+     def test_demo(self):
+         sku1 = Systembolaget(self.driver)
+         sku1.find_item("2722")
+         demoartikel = self.driver.find_element(By.XPATH, "//div[@class='css-8zpafe e3whs8q0']//p[@class='css-12l74ml er6ap680']")
+         demoartikel = demoartikel.text
+         assert "2722" in demoartikel
 
 
