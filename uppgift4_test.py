622e6f7201ec6101b5dbb41e0e88e3c9cafb67c1
helenghirmai <43412109+helenghirmai@users.noreply.github.com>
2023-01-13 23:36:26 +0100

    Added selenium webdriver

--- uppgift4_test.py
+++ uppgift4_test.py
@@ -3,7 +3,8 @@ import re
 import requests
 import time
 from uppgift4_program import Systembolaget
-from selenium.webdriver import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
