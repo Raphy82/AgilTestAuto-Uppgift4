0b9d946e5eacfb1d2edf05407f07598badb9b02e
helenghirmai <43412109+helenghirmai@users.noreply.github.com>
2023-01-13 12:16:49 +0100

    Added selenium.webdriver

--- uppgift4_program.py
+++ uppgift4_program.py
@@ -4,6 +4,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Keys
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
