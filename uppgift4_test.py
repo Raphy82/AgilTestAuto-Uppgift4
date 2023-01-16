82255d3c309fedaaed8b9b2346a0bf58bbd2c612
helenghirmai <43412109+helenghirmai@users.noreply.github.com>
2023-01-16 12:38:06 +0100

    Added import Service as ChromeService

--- uppgift4_test.py
+++ uppgift4_test.py
@@ -6,6 +6,7 @@ from uppgift4_program import Systembolaget
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
