a4c0cc859e31e64e3846c8ce2995aafd708a2ab8
helenghirmai <43412109+helenghirmai@users.noreply.github.com>
2023-01-17 11:27:10 +0100

    added from common keys

--- uppgift4_test.py
+++ uppgift4_test.py
@@ -6,7 +6,7 @@ from uppgift4_program import Systembolaget
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
