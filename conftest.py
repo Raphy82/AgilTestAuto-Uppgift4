4a6f77632ec28e0545ecd5b2fa7f01dec2bacc22
helenghirmai <43412109+helenghirmai@users.noreply.github.com>
2023-01-17 10:55:29 +0100

    fix failure Service as ChromeService

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://www.systembolaget.se")
     driver.fullscreen_window()
     driver.implicitly_wait(10)
